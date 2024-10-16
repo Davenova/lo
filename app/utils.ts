3d5daff5bdef96894985b8f8137c6b6f0601ea20
Unicorn X <dudebrothers2@gmail.com>
2024-10-17 01:56:01 +0530

    Create utils.ts

---
+++ app/utils.ts
@@ -0,0 +1,14 @@
+export const getRandomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const texts = [
+  "Exciting updates are on the way, keep farming :)",
+  "Stay healthy and stay alive for future events :)",
+  "Join our socials for latest updates",
+  "Only follow news shared on our official platforms",
+  "You’re part of something special, stay connected for surprises!",
+  "Big things are growing, keep your eyes on the field!",
+  "Hold tight! The best surprises are worth the wait!",
+  "Behind the scenes, we’re working on something you’ll love!"
+];
