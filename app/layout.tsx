7fee08f84e157cb4c8e846957fb2e4f691113c41
Unicorn X <dudebrothers2@gmail.com>
2024-10-17 02:45:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
       </Head>
       <body className={inter.className}>
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
