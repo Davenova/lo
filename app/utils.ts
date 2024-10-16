72857aab7aa69562f3d5998d880fae1854f7084f
Unicorn X <dudebrothers2@gmail.com>
2024-10-17 02:08:39 +0530

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -1,14 +1,35 @@
-export const getRandomInt = (min: number, max: number) => {
+function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
-export const texts = [
-  "Exciting updates are on the way, keep farming :)",
-  "Stay healthy and stay alive for future events :)",
-  "Join our socials for latest updates",
-  "Only follow news shared on our official platforms",
-  "You’re part of something special, stay connected for surprises!",
-  "Big things are growing, keep your eyes on the field!",
-  "Hold tight! The best surprises are worth the wait!",
-  "Behind the scenes, we’re working on something you’ll love!"
-];
+export function toggleUpdateText() {
+  const updateTextElement = document.getElementById('updateText');
+  const texts = [
+    "Exciting updates are on the way, keep farming :)",
+    "Stay healthy and stay alive for future events :)",
+    "Join our socials for latest updates",
+    "Only follow news shared in our official platforms",
+    "You’re part of something special, stay connected for surprises!",
+    "Big things are growing, keep your eyes on the field!",
+    "Hold tight! The best surprises are worth the wait!",
+    "Behind the scenes, we’re working on something you’ll love!"
+  ];
+  let previousIndex = -1;
+
+  setInterval(() => {
+    let randomIndex;
+    do {
+      randomIndex = getRandomInt(0, texts.length - 1);
+    } while (randomIndex === previousIndex);
+
+    previousIndex = randomIndex;
+
+    updateTextElement.classList.remove('fade-in');
+    updateTextElement.classList.add('fade-out');
+    setTimeout(() => {
+      updateTextElement.textContent = texts[randomIndex];
+      updateTextElement.classList.remove('fade-out');
+      updateTextElement.classList.add('fade-in');
+    }, 1000);
+  }, 15000);
+}
