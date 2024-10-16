6ab6180216fce08b389c7ba370b1a94a304fe54a
Unicorn X <dudebrothers2@gmail.com>
2024-10-17 01:16:30 +0530

    Update route.ts

--- app/api/increase-points/route.ts
+++ app/api/increase-points/route.ts
@@ -10,7 +10,10 @@ export async function POST(req: NextRequest) {
         }
 
         // Define claimed field based on buttonId
-        const claimedField = buttonId === 'button1' ? 'claimedButton1' : 'claimedButton2' : 'claimedButton3';
+        const claimedField = 
+            buttonId === 'button1' ? 'claimedButton1' : 
+            buttonId === 'button2' ? 'claimedButton2' : 
+            buttonId === 'button3' ? 'claimedButton3';
 
         const updatedUser = await prisma.user.update({
             where: { telegramId },
