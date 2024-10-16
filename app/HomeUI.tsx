21eaec406aedd23a6c73de5ada41f5abe99e5f35
Unicorn X <dudebrothers2@gmail.com>
2024-10-17 02:06:08 +0530

    Update HomeUI.tsx

--- app/HomeUI.tsx
+++ app/HomeUI.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { getRandomInt, texts } from './utils'; // Importing utility functions
+import { useEffect } from 'react';
 
 interface HomeUIProps {
   user: any;
@@ -31,129 +30,123 @@ export default function HomeUI({
   handleClaim2,
   handleClaim3
 }: HomeUIProps) {
-  const [updateText, setUpdateText] = useState("Exciting updates are on the way, keep farming :)");
-
   useEffect(() => {
+    toggleUpdateText(); // Call the function from the script
+  }, []);
+
+  function getRandomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function toggleUpdateText() {
+    const updateTextElement = document.getElementById('updateText');
+    const texts = [
+      "Exciting updates are on the way, keep farming :)",
+      "Stay healthy and stay alive for future events :)",
+      "Join our socials for latest updates",
+      "Only follow news shared in our official platforms",
+      "You’re part of something special, stay connected for surprises!",
+      "Big things are growing, keep your eyes on the field!",
+      "Hold tight! The best surprises are worth the wait!",
+      "Behind the scenes, we’re working on something you’ll love!"
+    ];
     let previousIndex = -1;
 
-    const intervalId = setInterval(() => {
+    setInterval(() => {
       let randomIndex;
       do {
         randomIndex = getRandomInt(0, texts.length - 1);
       } while (randomIndex === previousIndex);
 
       previousIndex = randomIndex;
-      setUpdateText(texts[randomIndex]);
-    }, 15000);
 
-    return () => clearInterval(intervalId); // Cleanup interval on component unmount
-  }, []);
+      if (updateTextElement) {
+        updateTextElement.classList.remove('fade-in');
+        updateTextElement.classList.add('fade-out');
+        setTimeout(() => {
+          updateTextElement.textContent = texts[randomIndex];
+          updateTextElement.classList.remove('fade-out');
+          updateTextElement.classList.add('fade-in');
+        }, 1000);
+      }
+    }, 15000);
+  }
 
   return (
     <div className="bg-gray-100 flex flex-col items-center justify-between min-h-screen">
-      {/* Header */}
-      <div className="bg-white w-full h-3/4 rounded-b-full flex flex-col items-center justify-center shadow-lg">
-        <div className="bg-gray-300 w-24 h-24 rounded-full mb-4">
-          <img alt="Profile" className="w-full h-full rounded-full object-cover" src="https://storage.googleapis.com/a1aa/image/YlpvEfbklKRiDi8LX5Rww5U3zZZwHEUfju1qUNknpEZ6e2OnA.jpg" />
+      <div className="bg-gray-200 w-full h-1/2 rounded-b-full flex flex-col items-center justify-center shadow-lg">
+        <div className="bg-gray-300 w-32 h-32 rounded-full shadow-md drag-down">
+          <img alt="Animated style dog image" className="w-full h-full rounded-full object-cover" src="https://storage.googleapis.com/a1aa/image/YlpvEfbklKRiDi8LX5Rww5U3zZZwHEUfju1qUNknpEZ6e2OnA.jpg" />
         </div>
-        <p className="text-gray-800 text-2xl font-bold mb-12 mt-4">{user.points} PixelDogs</p>
-
-        {/* Tasks Section */}
-        <div className="bg-white w-10/12 pt-6 pb-6 px-12 rounded-lg flex flex-col items-center shadow-md mt-12">
-          <div className="bg-gray-200 text-center py-2 rounded-full mb-4 w-2/3 mx-auto">
-            <p className="font-bold text-gray-600">Daily Tasks..!</p>
-          </div>
-
-          {/* Button 1 */}
-          <div className="bg-gray-200 w-full p-4 rounded-lg flex justify-between items-center mb-4 glow-pink-on-hover transition duration-300">
-            <p className="text-gray-800">Follow Our Youtube!</p>
+        <p className="text-gray-700 text-3xl mt-4 font-bold">{user.points} PixelDogs</p>
+        <div className="bg-gray-100 w-3/4 mt-4 p-6 rounded-lg flex flex-col items-center shadow-md">
+          <button className="bg-gray-200 text-gray-700 px-6 py-2 rounded-full mb-4 font-semibold shadow-sm hover:bg-gray-300 transition duration-300">Daily Tasks..!</button>
+          <div className="bg-gray-200 w-full p-4 rounded-lg flex justify-between items-center mb-4 shadow-sm glow-on-hover transition duration-300">
+            <p className="text-gray-700 font-medium">Follow Our Youtube!</p>
             <button
               onClick={() => {
                 if (buttonStage1 === 'check') {
-                  handleButtonClick1()
+                  handleButtonClick1();
                 } else if (buttonStage1 === 'claim') {
-                  handleClaim1()
+                  handleClaim1();
                 }
               }}
               disabled={buttonStage1 === 'claimed' || isLoading}
-              className={`bg-gray-300 text-gray-800 px-4 py-2 rounded-lg ${
+              className={`bg-gray-300 text-gray-700 px-4 py-1 rounded-full font-semibold shadow-sm hover:bg-gray-400 transition duration-300 ${
                 buttonStage1 === 'claimed' || isLoading ? 'cursor-not-allowed' : ''
               }`}
             >
               {isLoading ? 'Claiming...' : buttonStage1 === 'check' ? 'Check' : buttonStage1 === 'claim' ? 'Claim' : 'Claimed'}
             </button>
           </div>
-
-          {/* Button 2 */}
-          <div className="bg-gray-200 w-full p-4 rounded-lg flex justify-between items-center mb-4 glow-green-on-hover transition duration-300">
-            <p className="text-gray-800">Follow Our Twitter!</p>
+          <div className="bg-gray-200 w-full p-4 rounded-lg flex justify-between items-center mb-4 shadow-sm glow-green-on-hover transition duration-300">
+            <p className="text-gray-700 font-medium">Follow Our Twitter!</p>
             <button
               onClick={() => {
-                if (buttonStage2 === 'check') {
-                  handleButtonClick2();
-                } else if (buttonStage2 === 'claim') {
-                  handleClaim2();
-                }
+                handleButtonClick2();
+                handleClaim2();
               }}
-              disabled={buttonStage2 === 'claimed' || isLoading}
-              className={`bg-gray-300 text-gray-800 px-4 py-2 rounded-lg ${
-                buttonStage2 === 'claimed' || isLoading ? 'cursor-not-allowed' : ''
-              }`}
+              disabled={buttonStage2 === 'claimed'}
+              className="bg-gray-300 text-gray-700 px-4 py-1 rounded-full font-semibold shadow-sm hover:bg-gray-400 transition duration-300"
             >
-              {isLoading ? 'Claiming...' : buttonStage2 === 'check' ? 'Check' : buttonStage2 === 'claim' ? 'Claim' : 'Claimed'}
+              {buttonStage2 === 'check' ? 'Check' : buttonStage2 === 'claim' ? 'Claim' : 'Claimed'}
             </button>
           </div>
-
-          {/* Button 3 */}
-          <div className="bg-gray-200 w-full p-4 rounded-lg flex justify-between items-center mb-4 glow-blue-on-hover transition duration-300">
-            <p className="text-gray-800">Follow Our Telegram!</p>
+          <div className="bg-gray-200 w-full p-4 rounded-lg flex justify-between items-center mb-4 shadow-sm glow-blue-on-hover transition duration-300">
+            <p className="text-gray-700 font-medium">Join Our Telegram!</p>
             <button
               onClick={() => {
-                if (buttonStage3 === 'check') {
-                  handleButtonClick3();
-                } else if (buttonStage3 === 'claim') {
-                  handleClaim3();
-                }
+                handleButtonClick3();
+                handleClaim3();
               }}
-              disabled={buttonStage3 === 'claimed' || isLoading}
-              className={`bg-gray-300 text-gray-800 px-4 py-2 rounded-lg ${
-                buttonStage3 === 'claimed' || isLoading ? 'cursor-not-allowed' : ''
-              }`}
+              disabled={buttonStage3 === 'claimed'}
+              className="bg-gray-300 text-gray-700 px-4 py-1 rounded-full font-semibold shadow-sm hover:bg-gray-400 transition duration-300"
             >
-              {isLoading ? 'Claiming...' : buttonStage3 === 'check' ? 'Check' : buttonStage3 === 'claim' ? 'Claim' : 'Claimed'}
+              {buttonStage3 === 'check' ? 'Check' : buttonStage3 === 'claim' ? 'Claim' : 'Claimed'}
             </button>
           </div>
         </div>
       </div>
-
-      {/* Farm PixelDogs Button */}
-      <button className="bg-gray-800 text-white w-10/12 p-8 rounded-full mt-8 mb-4 shadow-lg hover:bg-gray-900 transition duration-300">
-        Farm PixelDogs...
-      </button>
-
-      {/* Update Text Section */}
-      <p id="updateText" className="text-gray-700 text-center mb-4 fade-in text-sm">
-        {updateText}
-      </p>
-
-      {/* Bottom Navigation */}
+      <div className="flex-grow"></div>
+      <button className="bg-gray-800 text-white w-3/4 py-4 rounded-full mb-6 font-semibold text-lg shadow-sm hover:bg-gray-900 transition duration-300">Farm PixelDogs...</button>
+      <p id="updateText" className="text-gray-700 text-center mb-4 fade fade-in text-sm">{notification}</p>
       <div className="bg-white w-full py-4 flex justify-around items-center shadow-t-lg">
         <Link href="/">
-          <a className="flex flex-col items-center text-gray-800">
+          <a className="flex flex-col items-center text-gray-700 hover:text-gray-900 transition duration-300">
             <i className="fas fa-home text-2xl"></i>
-            <p className="text-sm">Home</p>
+            <p className="font-medium">Home</p>
           </a>
         </Link>
         <Link href="/invite">
-          <a className="flex flex-col items-center text-gray-800">
+          <a className="flex flex-col items-center text-gray-700 hover:text-gray-900 transition duration-300">
             <i className="fas fa-users text-2xl"></i>
-            <p className="text-sm">Friends</p>
+            <p className="font-medium">Friends</p>
           </a>
         </Link>
         <Link href="/wallet">
-          <a className="flex flex-col items-center text-gray-800">
+          <a className="flex flex-col items-center text-gray-700 hover:text-gray-900 transition duration-300">
             <i className="fas fa-wallet text-2xl"></i>
-            <p className="text-sm">Wallet</p>
+            <p className="font-medium">Wallet</p>
           </a>
         </Link>
       </div>
