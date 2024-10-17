056db2bc1917a92033c1e9f3044f90f16b152a8a
Unicorn X <dudebrothers2@gmail.com>
2024-10-17 14:29:38 +0530

    Update page.tsx

--- app/invite/page.tsx
+++ app/invite/page.tsx
@@ -103,7 +103,7 @@ export default function Invite() {
               <path d="M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M9 11C11.2091 11 13 9.20914 13 7C13 4.79086 11.2091 3 9 3C6.79086 3 5 4.79086 5 7C5 9.20914 6.79086 11 9 11Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
-            <h2 className={styles.invitedTitle}>Invited Friends :{invitedUsers.length}</h2>
+            <h2 className={styles.invitedTitle}>Invited Friends : {invitedUsers.length}</h2>
           </div>
           {invitedUsers.length > 0 ? (
             <ul className={styles.invitedList}>
